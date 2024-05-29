9f3c55a6035f796b663c05c26ef607339a2d2375
alix.gao <alix.gao@qq.com>
2024-05-29 21:11:54 +0800

    feat: add error code
    Signed-off-by: alix.gao <alix.gao@qq.com>

--- cmd-1.sh
+++ cmd-1.sh
@@ -2,5 +2,6 @@ set -e
 
 sudo apt install cppcheck
 
-cppcheck . --suppress=*:src/main.cpp --suppress=*:other/*
+cppcheck . --suppress=*:src/main.cpp --suppress=*:other/* --error-exitcode=29
 
+echo "no error"
